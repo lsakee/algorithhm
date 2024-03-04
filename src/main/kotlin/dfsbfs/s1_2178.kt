f2dfda7d6b6407b1b2659cea2634504fd11e9d56
lsakee <93514333+lsakee@users.noreply.github.com>
2024-03-04 20:04:12 +0900

    solve : dfs & bfs s2 2178

--- src/main/kotlin/dfsbfs/s1_2178.kt
+++ src/main/kotlin/dfsbfs/s1_2178.kt
@@ -1,18 +1,18 @@
 package dfsbfs
 
 
-private lateinit var map: Array<Array<Int>>
+private lateinit var map: Array<IntArray>
 private fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
     val (n, m) = br.readLine().split(" ").map { it.toInt() }
     val dx = listOf(0, 1, 0, -1)
     val dy = listOf(1, 0, -1, 0)
-    map = Array(n) { Array(m) { 0 } }
-    repeat(n) { i ->
-        val input = br.readLine()
-        for (j in 0 until m) {
-            map[i][j] = input[j].toString().toInt()
+    map = Array(n) { IntArray(m) { 0 } }
+    repeat(n) { n ->
+        val a =br.readLine()
+        repeat(m) { m ->
+            map[n][m] = a[m].toString().toInt()
         }
     }
 
@@ -28,7 +28,7 @@ private fun main() {
                 val ny = tmpY + dy[i]
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue
                 if (map[nx][ny] == 1) {
-                    map[nx][ny] == map[tmpX][tmpY] + 1
+                    map[nx][ny] = map[tmpX][tmpY] + 1
                     queue.add(Node(nx, ny))
                 }
             }
@@ -45,5 +45,3 @@ data class Node(
     val x: Int,
     val y: Int
 )
-
-
